6edf611c2c0a23b6c8256fa09ab38ee63b54e6a8
Gian124a <140373193+Gian124a@users.noreply.github.com>
2023-09-18 19:14:59 -0400

    Update Funcion.js

--- Funcion.js
+++ Funcion.js
@@ -56,3 +56,18 @@ function Botoncito() {
     Boton.classList.toggle('Active');
     Boton2.classList.toggle('Active');
 }
+function CambiarPantalla(pantallaActual, pantallaSiguiente, circuloSiguiente) {
+    const pantallaActualElemento = document.getElementById(pantallaActual);
+    const pantallaSiguienteElemento = document.getElementById(pantallaSiguiente);
+    const circuloSiguienteElemento = document.getElementById(circuloSiguiente);
+
+    circuloSiguienteElemento.style.background = 'gray';
+    pantallaActualElemento.style.display = 'none';
+    pantallaSiguienteElemento.style.display = 'grid';
+}
+function DevolverPantalla(pantallaActual, pantallaAtras) {
+    const pantallaActualElemento = document.getElementById(pantallaActual);
+    const pantallaSiguienteElemento = document.getElementById(pantallaAtras);
+    pantallaActualElemento.style.display = 'none';
+    pantallaSiguienteElemento.style.display = 'grid';
+}
